799b3200fac6f81ddd9507945940750fab0b2f35
savannah8 <willengitonga.wg@gmail.com>
2019-10-06 09:57:38 +0300

    finished styling

--- fashion/settings.py
+++ fashion/settings.py
@@ -3,10 +3,7 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-SECRET_KEY = '@$nbgnpufae^pn-fu2@g3=0k&8d+ysxxpa)*87k%83)2a0w-f2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
